ae4e8aa4f210c7135dfca8fcdf003b9cd19ee281
shmd001 <cool.shoronov19@gmail.com>
2023-03-06 21:14:47 +0300

    Configured React app

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
     return (
         <div className="App">
-            <h1>Hello</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
 }

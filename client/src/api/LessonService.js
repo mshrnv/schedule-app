0db754e6f3e82f0268e77a88a4b9e4ffd9390faa
shmd001 <cool.shoronov19@gmail.com>
2023-03-07 09:52:14 +0300

    Added `LessonService`

---
+++ client/src/api/LessonService.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+
+export default class LessonService {
+    static apiUrl = "http://localhost:5000/lessons"
+    static async getLessonsByDate(date) {
+        const response = await axios.get(this.apiUrl, {
+            date
+        })
+        return response;
+    }
+
+    static async newLesson(data) {
+        const response = await axios.post(this.apiUrl, {
+            data
+        })
+        return response;
+    }
+
+    static async editLesson(filter, update) {
+        const response = await axios.put(this.apiUrl, {
+            filter,
+            update
+        })
+        return response;
+    }
+
+    static async deleteLesson(filter) {
+        const response = await axios.delete(this.apiUrl, {
+            data: {
+                filter
+            }
+        })
+        return response;
+    }
+}
+
+
+
